26adc84c69698699ef1447e2f1078fc08ab77509
lasttrader <grishaperm@list.ru>
2024-08-18 09:51:22 +0300

    added baseapp with models

--- baseApp.py
+++ baseApp.py
@@ -5,7 +5,7 @@ from sklearn.neighbors import KNeighborsClassifier
 print('import success')
 
 #load_models
-
+#le
 job_LE = pickle.load(open('../180824_prod/models/job_LE.pkl', 'rb'))
 marital_LE = pickle.load(open('../180824_prod/models/marital_LE.pkl', 'rb')) 	
 education_LE = pickle.load(open('../180824_prod/models/education_LE.pkl', 'rb'))	
@@ -15,7 +15,14 @@ loan_LE	= pickle.load(open('../180824_prod/models/loan_LE.pkl', 'rb'))
 contact_LE	= pickle.load(open('../180824_prod/models/contact_LE.pkl', 'rb'))
 month_LE	= pickle.load(open('../180824_prod/models/month_LE.pkl', 'rb'))
 poutcome_LE	= pickle.load(open('../180824_prod/models/poutcome_LE.pkl', 'rb'))
+
 y_LE = pickle.load(open('../180824_prod/models/y_LE.pkl', 'rb'))
+
+#scaler
+num_scaler = pickle.load(open('../180824_prod/models/num_scaler.pkl', 'rb'))
+
+#ML models
+kNN = pickle.load(open('../180824_prod/models/kNN.pkl', 'rb'))
 
 #get_data
 #вводим с клавиатуры
@@ -38,6 +45,10 @@ y_LE = pickle.load(open('../180824_prod/models/y_LE.pkl', 'rb'))
 # pdays	 = float(input())
 # previous = float(input())
 
+
+
+#preprocessing
+##categorical
 X_cat_from_keyboard = ['unemployed',
                        'married',
                        'primary',
@@ -48,7 +59,27 @@ X_cat_from_keyboard = ['unemployed',
                        'may',
                        'failure'
                         ]
-print(X_cat_from_keyboard)
+# print(X_cat_from_keyboard)
+
+le_list = [job_LE,	
+           marital_LE,	
+           education_LE,	
+           default_LE,
+           housing_LE,
+           loan_LE,
+           contact_LE,
+           month_LE,
+           poutcome_LE]
+
+X_le_list = [] #под закодированные признаки
+for i in range(len(X_cat_from_keyboard)):
+    x_cat = le_list[i].transform([X_cat_from_keyboard[i]])[0]
+    # print(x_cat)
+    X_le_list.append(x_cat)
+
+print('X_cat_le:', X_le_list)
+
+##num
 X_nums_from_keyboard =[30,
                        1787,
                        16,
@@ -56,17 +87,23 @@ X_nums_from_keyboard =[30,
                        4,
                        330,
                        0]
-print(X_nums_from_keyboard)
-
-#preprocessing
-##categorical
-
-##num
+print('X_nums', X_nums_from_keyboard)
 
 ##объединить категориальные и числовые (в том же порядке, как и при обучении)
 
+X = []
+X.extend(X_le_list)
+X.extend(X_nums_from_keyboard)
+print('X:', X)
+
 #scaler
+X_scaled = num_scaler.transform([X])
+print('X_scaled:', X_scaled)
 
 #predict
+prediction = kNN.predict(X_scaled)
+print(prediction)
 
 #result
+result = y_LE.inverse_transform(prediction)
+print('Выдавать клиентский договор? Ответ - ', result)
